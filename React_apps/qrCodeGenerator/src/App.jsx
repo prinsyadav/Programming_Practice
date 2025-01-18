76ebf2a323a5fcf38bb0e3c8d53e22b525f7bdbe
Prince Yadav <asdfghjkl890890@proton.me>
2025-01-18 14:57:04 +0530

    Create QrCodeGenerator

--- React_apps/qrCodeGenerator/src/App.jsx
+++ React_apps/qrCodeGenerator/src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [temp, setTemp] = useState("");
@@ -8,16 +8,12 @@ function App() {
   const [bgColor, setBgColor] = useState("ffffff");
   const [qrCode, setQrCode] = useState("");
 
-  // Changing the URL only when the user
-  // changes the input
   useEffect(() => {
     setQrCode(
       `http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`
     );
   }, [word, size, bgColor]);
 
-  // Updating the input word when user
-  // click on the generate button
   function handleClick() {
     setWord(temp);
   }
@@ -68,4 +64,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
